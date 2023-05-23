e59dc2d8e0226b50c3787d6c0d56d9bdb0352743
PedrorOliva <pedro.robson@zup.com.br>
2023-05-23 17:31:46 -0300

    modificando exercicio 2

--- src/ExercicioDois.java
+++ src/ExercicioDois.java
@@ -15,8 +15,27 @@ public class ExercicioDois {
 
     float imc = peso / (altura * altura);
 
-    System.out.printf("O seu imc é: " + "%.2f", imc);
-
-
+    if(imc <= 16.9) {
+      System.out.printf("O seu imc é: " + "%.2f", imc);
+      System.out.println(" -> Muito abaixo do peso!");
+    } else if (imc >= 18 && imc <= 18.4 ) {
+      System.out.printf("O seu imc é: " + "%.2f", imc);
+      System.out.println(" -> Abaixo do peso!");
+    } else if (imc >= 18.5 && imc <= 24.9) {
+      System.out.printf("O seu imc é: " + "%.2f", imc);
+      System.out.println(" -> Peso normal!");
+    } else if (imc >= 25 && imc <= 29.9 ) {
+      System.out.printf("O seu imc é: " + "%.2f", imc);
+      System.out.println(" -> Acima do peso!");
+    } else if (imc >= 30 && imc <= 34.9) {
+      System.out.printf("O seu imc é: " + "%.2f", imc);
+      System.out.println(" -> Obesidade grau 1!");
+    } else if (imc >= 35 && imc <= 40) {
+      System.out.printf("O seu imc é: " + "%.2f", imc);
+      System.out.println(" -> Obesidade grau 3!");
+    } else {
+      System.out.printf("O seu imc é: " + "%.2f", imc);
+      System.out.println(" -> Obesidade grau 3!");
+    }
   }
 }
